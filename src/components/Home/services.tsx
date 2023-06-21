2941d971b1be89e706810fdc894e5b58595d8ae5
OKoro Gift <kristagift@gmail.com>
2023-06-21 22:48:30 +0100

    Import icon

--- src/components/Home/services.tsx
+++ src/components/Home/services.tsx
@@ -6,27 +6,43 @@ import Share from "../../assets/Share_Icon.svg";
 
 function Services() {
   return (
-    <div className="flex">
-      <div>
-        <img src={Hospital} alt="Nurse and patient" className="" />
-        <h2>Search Hospitalss</h2>
-        <p>Effortlessly Find the Best Hospitals Near You</p>
+    <div className="my-[50px] mx-[15px] md:mx-[2px] w-full ">
+      <div className="flex flex-col md:flex-row justify-center gap-[20px]  ">
+        <div
+          style={{ backgroundColor: "#E0E4EC" }}
+          className="w-[345px] rounded-xl p-[20px]"
+        >
+          <img src={Hospital} alt="Nurse and patient" className="mx-[100px]" />
+
+          <h2 className="font-bold my-[13px]">Search Hospitals</h2>
+          <p>Effortlessly Find the Best Hospitals Near You</p>
+        </div>
+        <div
+          style={{ backgroundColor: "#E0E4EC" }}
+          className="w-[345px] rounded-xl  p-[20px]"
+        >
+          <img src={Doctor} alt="Nurse and patient" className="mx-[100px] " />
+          <h2 className="font-bold my-[13px]">Search Doctors</h2>
+          <p>Effortlessly Find the Best Doctors Near You</p>
+        </div>
+        <div
+          style={{ backgroundColor: "#E0E4EC" }}
+          className="w-[345px] rounded-xl  p-[20px]"
+        >
+          <img src={Export} alt="Nurse and patient" className="mx-[100px] " />
+          <h2 className="font-bold my-[13px]">Export Doctors</h2>
+          <p>Save list of hospitals.</p>
+        </div>
+        <div
+          style={{ backgroundColor: "#E0E4EC" }}
+          className="w-[345px]  rounded-xl  p-[20px]"
+        >
+          <img src={Share} alt="Nurse and patient" className="mx-[100px]" />
+          <h2 className="font-bold my-[13px]">Share Doctors</h2>
+          <p>Share the list of hospitals with others. </p>
+        </div>
       </div>
-      <div>
-        <img src={Doctor} alt="Nurse and patient" className="]" />
-        <h2>Search Doctors</h2>
-        <p>Effortlessly Find the Best Doctors Near You</p>
-      </div>
-      <div>
-        <img src={Export} alt="Nurse and patient" className="" />
-        <h2>Export Doctors</h2>
-        <p>Save list of hospitals.</p>
-      </div>
-      <div>
-        <img src={Share} alt="Nurse and patient" className="" />
-        <h2>Share Doctors</h2>
-        <p>Share the list of hospitals with others. </p>
-      </div>
+      <div></div>
     </div>
   );
 }
